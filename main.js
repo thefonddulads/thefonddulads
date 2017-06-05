380473224e38cb200146ad4dcbb880f34462e06b
Maximilian Holzmann <max@holzmann.io>
2017-06-05 12:58:24 -0500

    finishes adding characters to js

--- main.js
+++ main.js
@@ -69,5 +69,69 @@ switch(k) {
   title.innerHTML = "Cult Minion";
   info.innerHTML = "<img class='castlepic' src='images/castle/cultminion.png'> <p class='biotext'>This is the cult minion's bio.</p>";
   break;
+  case 'fencer':
+  title.innerHTML = "Fencer";
+  info.innerHTML = "<img class='castlepic' src='images/castle/fencer.png'> <p class='biotext'>This is the fencer's bio.</p>";
+  break;
+  case 'firedemon':
+  title.innerHTML = "Fire Demon";
+  info.innerHTML = "<img class='castlepic' src='images/castle/firedemon.png'> <p class='biotext'>This is the fire demon's bio.</p>";
+  break;
+  case 'hatty':
+  title.innerHTML = "Hatty Hattington";
+  info.innerHTML = "<img class='castlepic' src='images/castle/hatty.png'> <p class='biotext'>This is the Hatty Hattington's bio.</p>";
+  break;
+  case 'iceskimo': 
+  title.innerHTML = "Iceskimo";
+  info.innerHTML = "<img class='castlepic' src='images/castle/iceskimo.png'> <p class='biotext'>This is the iceskimo's bio.</p>";
+  break;
+  case 'industrialist':
+  title.innerHTML = "Industrialist";
+  info.innerHTML = "<img class='castlepic' src='images/castle/industrialist.png'> <p class='biotext'>This is the industrialist's bio.</p>";
+  break;
+  case 'beekeeper':
+  title.innerHTML = "Killer Beekeeper";
+  info.innerHTML = "<img class='castlepic' src='images/castle/beekeeper.png'> <p class='biotext'>This is the killer beekeeper's bio.</p>";
+  break;
+  case 'king':
+  title.innerHTML = "King";
+  info.innerHTML = "<img class='castlepic' src='images/castle/king.png'> <p class='biotext'>This is the king's bio.</p>";
+  break;
+  case 'necromancer':
+  title.innerHTML = "Necromancer";
+  info.innerHTML = "<img class='castlepic' src='images/castle/necromancer.png'> <p class='biotext'>This is the necromancer's bio.</p>";
+  break;
+  case 'ninja':
+  title.innerHTML = "Ninja";
+  info.innerHTML = "<img class='castlepic' src='images/castle/ninja.png'> <p class='biotext'>This is the ninja's bio.</p>";
+  break;
+  case 'peasant':
+  title.innerHTML = "Peasant";
+  info.innerHTML = "<img class='castlepic' src='images/castle/peasant.png'> <p class='biotext'>This is the peasant's bio.</p>";
+  break;
+  case 'royalguard':
+  title.innerHTML = "Royal Guard";
+  info.innerHTML = "<img class='castlepic' src='images/castle/royalguard.png'> <p class='biotext'>This is the royal guard's bio.</p>";
+  break;
+  case 'saracen':
+  title.innerHTML = "Saracen";
+  info.innerHTML = "<img class='castlepic' src='images/castle/saracen.png'> <p class='biotext'>This is the saracen's bio.</p>";
+  break;
+  case 'skeleton':
+  title.innerHTML = "Skeleton";
+  info.innerHTML = "<img class='castlepic' src='images/castle/skeleton.png'> <p class='biotext'>This is the skeleton's bio.</p>";
+  break;
+  case 'snakey':
+  title.innerHTML = "Snakey";
+  info.innerHTML = "<img class='castlepic' src='images/castle/snakey.png'> <p class='biotext'>This is the snakey's bio.</p>";
+  break;
+  case 'stoveface':
+  title.innerHTML = "Stove Face";
+  info.innerHTML = "<img class='castlepic' src='images/castle/stove.png'> <p class='biotext'>This is the stove face's bio.</p>";
+  break;
+  case 'thief':
+  title.innerHTML = "Thief";
+  info.innerHTML = "<img class='castlepic' src='images/castle/thief.png'> <p class='biotext'>This is the cult thief's bio.</p>";
+  break;
  }
 }
