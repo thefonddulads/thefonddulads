8f3f87ae6b9e120a65934e081b02246ff0f48f66
MaxHolzmann <max@holzmann.io>
2017-06-04 22:24:05 -0500

    pushes cc update

--- main.js
+++ main.js
@@ -3,16 +3,37 @@ function tier(){
 var k = document.getElementById('in').value;
 var title = document.getElementById('char');
 var info = document.getElementById('info');
-if (k == "char") {
-title.innerHTML = "Select a character!";
-info.innerHTML = " ";
-}
-else if(k == "red") {
-title.innerHTML = "Red Knight";
-info.innerHTML = "<img class='castlepic' src='images/castle/red.png'>  <p class='biotext'>This is the red knight's bio.</p>";
-}
-else if (k == "blue") {
-title.innerHTML = "Blue Knight";
-info.innerHTML = "<img class='castlepic' src='images/castle/blue.png'> <p class='biotext'>This is the blue knight's bio.</p>"
+
+switch(k) {
+  case 'char':
+  title.innerHTML = "Select a character!";
+  info.innerHTML = " ";
+  break;
+  case 'red':
+  title.innerHTML = "Red Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/red.png'>  <p class='biotext'>This is the red knight's bio.</p>";
+  break;
+  case 'blue':
+  title.innerHTML = "Blue Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/blue.png'> <p class='biotext'>This is the blue knight's bio.</p>";
+  break;
+  case 'green':
+  title.innerHTML = "Green Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/green.png'> <p class='biotext'>This is the green knight's bio.</p>";
+  case 'orange':
+  title.innerHTML = "Orange Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/orange.png'> <p class='biotext'>This is the orange knight's bio.</p>";
+  break;
+  case 'gray':
+  title.innerHTML = "Gray Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/gray.png'> <p class='biotext'>This is the gray knight's bio.</p>";
+  break;
+  case 'opengray':
+  title.innerHTML = "Open Face Gray Knight";
+  info.innerHTML = "<img class='castlepic' src='images/castle/opengray.png'> <p class='biotext'>This is the open face gray knight's bio.</p>";
+  break;
+  case 'blacksmith':
+  title.innerHTML = "Blacksmith";
+  info.innerHTML = "<img class='castlepic' src='images/castle/blacksmith.png'> <p class='biotext'>This is the blacksmith's bio.</p>";
 }
 }
