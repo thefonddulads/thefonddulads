7f63a976508138bbff16ec4786ac8d427b5c4cc8
MaxHolzmann <max@holzmann.io>
2017-06-04 21:37:30 -0500

    tool update

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// JavaScript file! 
+// JavaScript file!
+function tier(){
+var k = document.getElementById('in').value;
+var title = document.getElementById('char');
+var info = document.getElementById('info');
+if (k == "char") {
+title.innerHTML = "Select a character!";
+info.innerHTML = " ";
+}
+else if(k == "red") {
+title.innerHTML = "Red Knight";
+info.innerHTML = "<img class='castlepic' src='images/castle/red.png'>  <p class='biotext'>This is the red knight's bio.</p>";
+}
+else if (k == "blue") {
+title.innerHTML = "Blue Knight";
+info.innerHTML = "<img class='castlepic' src='images/castle/blue.png'> <p class='biotext'>This is the blue knight's bio.</p>"
+}
+}
